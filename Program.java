1e3149a79b5347e6d5f4d4bb57f1785e3ab56159
kpkr914 <49895586+kpkr914@users.noreply.github.com>
2019-04-25 11:37:20 +0530

    Update Program.java

--- Program.java
+++ Program.java
@@ -2,5 +2,6 @@ class Program {
  public static void main(String args[]){
      System.out.println("Hello Praveen");
      System.out.println("Welcome to Jenkins");
+     System.out.println("this is my first study on devops");
  }
 }
